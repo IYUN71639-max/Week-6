19c53e08d4ffb44fb84f9446ae0706eae6a6ba7f
IYUN71639-max <iyun@stu.socsd.org>
2024-10-09 09:46:51 -0400

    Create Conditionals.py

---
+++ Conditionals.py
@@ -0,0 +1,30 @@
+#Isaac Y.
+#October 7
+#Lesson on Conditionals
+a = (10 == 5)
+print(a)
+print(3 <= 7)
+print(3!=7)
+x=1
+b=(x>10)
+print(b)
+x=4
+print((x<6)or(x>2))
+a=(x>10) or (x%2==0)
+print(a)
+b=not(x<6)
+print(b)
+print(True or False)
+print(True and False)
+result=3+2*4<14 or 3==5
+print(result)
+x=5
+if x > 0:
+    print(x)
+    print("x is positive")
+print("outside of block")
+x=4
+if x % 2 == 0:
+    print("x is even")
+if x > 0:
+    print("x is positive")
